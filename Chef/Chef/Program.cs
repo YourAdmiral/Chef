dcfbce1e830b7b780f970e9e3ccaa1b5e0b5eeee
MANDOM <yourmarshall@yandex.by>
2020-12-05 19:48:15 +0300

    Создана папка с проектом

---
+++ Chef/Chef/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
