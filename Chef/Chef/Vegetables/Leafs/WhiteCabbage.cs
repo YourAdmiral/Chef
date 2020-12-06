6f5a162931217a60e5c2be4390ae0ab8e6d67b8b
MANDOM <yourmarshall@yandex.by>
2020-12-06 15:09:13 +0300

    Реализован класс WhiteCabbage
    Класс был создан до этого, но из-за ошибки компиляции оставался пустым.

--- Chef/Chef/Vegetables/Leafs/WhiteCabbage.cs
+++ Chef/Chef/Vegetables/Leafs/WhiteCabbage.cs
@@ -4,8 +4,33 @@ using System.Text;
 
 namespace Chef.Vegetables.Leafs
 {
-    class WhiteCabbage
+    class WhiteCabbage : Leaf
     {
-
+        double co;
+        public double Co { get; set; }
+        double mo;
+        public double Mo { get; set; }
+        double cr;
+        public double Cr { get; set; }
+        public WhiteCabbage(string sort, string color, double weight, double proteins, double fats, double carbohydrates, double calories, double water, double co, double mo, double cr)
+            : base(sort, color, weight, proteins, fats, carbohydrates, calories, water)
+        {
+            this.co = co;
+            this.mo = mo;
+            this.cr = cr;
+        }
+        public override void GetInformation()
+        {
+            base.GetInformation();
+            Console.WriteLine("Минералы и витамины:\n" +
+                "Оксид углерода - " + Co + " мг.\n" +
+                "Молибден - " + Mo + " мг.\n" +
+                "Хром - " + Cr + " мг.");
+        }
+        public override void Handle()
+        {
+            base.Handle();
+            Console.WriteLine("Нашинковали капусту...");
+        }
     }
 }
