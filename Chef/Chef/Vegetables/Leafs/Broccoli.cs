1e9efd9f2ce7e7959f9ba54a491aa44e22e49869
MANDOM <yourmarshall@yandex.by>
2020-12-06 15:15:03 +0300

    Реализован класс Broccoli
    Класс был создан до этого, но из-за ошибки компиляции оставался пустым.

--- Chef/Chef/Vegetables/Leafs/Broccoli.cs
+++ Chef/Chef/Vegetables/Leafs/Broccoli.cs
@@ -4,8 +4,33 @@ using System.Text;
 
 namespace Chef.Vegetables.Leafs
 {
-    class Broccoli
+    class Broccoli : Leaf
     {
-
+        double fe;
+        public double Fe { get; set; }
+        double i;
+        public double I { get; set; }
+        double mn;
+        public double Mn { get; set; }
+        public Broccoli(string sort, string color, double weight, double proteins, double fats, double carbohydrates, double calories, double water, double fe, double i, double mn)
+            : base(sort, color, weight, proteins, fats, carbohydrates, calories, water)
+        {
+            this.fe = fe;
+            this.i = i;
+            this.mn = mn;
+        }
+        public override void GetInformation()
+        {
+            base.GetInformation();
+            Console.WriteLine("Минералы и витамины:\n" +
+                "Железо - " + Fe + " мг.\n" +
+                "Йод - " + I + " мг.\n" +
+                "Марганец - " + Mn + " мг.");
+        }
+        public override void Handle()
+        {
+            base.Handle();
+            Console.WriteLine("Нашинковали брокколи...");
+        }
     }
 }
