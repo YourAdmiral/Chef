415348c067d6f17339dc88c6f85074d883003e5a
MANDOM <yourmarshall@yandex.by>
2020-12-06 17:40:37 +0300

    Создана классификация овощей: зеленые
    Был создан абстрактный класс Leaf унаследованный от класса Vegetable, представляющий собой овощи относящиеся к типу "Зеленые".

---
+++ Chef/Chef/Vegetables/Greens/Green.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chef.Vegetables.Greens
+{
+    abstract class Green : Vegetable
+    {
+        public Green(string sort, string color, double weight, double proteins, double fats, double carbohydrates, double calories, double water)
+            : base(sort, color, weight, proteins, fats, carbohydrates, calories, water)
+        {
+
+        }
+        public override void Handle()
+        {
+            base.Handle();
+            Console.WriteLine("Нарезали стебли...");
+        }
+
+        public override void GetInformation()
+        {
+            base.GetInformation();
+            Console.WriteLine("Классификация:\nЗеленое.");
+        }
+    }
+}
