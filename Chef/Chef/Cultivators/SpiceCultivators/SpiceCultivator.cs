97554ad2f3ca79d1929187594dd0d2826e370435
MANDOM <yourmarshall@yandex.by>
2020-12-06 23:15:21 +0300

    Создана фабрика специй
    На основе абстрактного класса Cultivator был создан абстрактный класс SpiceCultivator, отвечающий за создание объетов специй, и содержащий в себе метод CultivateSpice, который обязаны переопределить фабрики конкретных объектов, такие как: SaltCultivator, SugarCultivator, VinegarCultivator.

---
+++ Chef/Chef/Cultivators/SpiceCultivators/SpiceCultivator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chef.Cultivators.SpiceCultivators
+{
+    abstract class SpiceCultivator : Cultivator
+    {
+        public abstract Vegetables.Spices.Spice CultivateSpice();
+    }
+}
---
+++ Chef/Chef/Cultivators/SpiceCultivators/VinegarCultivator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chef.Cultivators.SpiceCultivators
+{
+    class VinegarCultivator : SpiceCultivator
+    {
+        public override Vegetables.Spices.Spice CultivateSpice()
+        {
+            return new Vegetables.Spices.Vinegar
+                (
+                WriteSort(),
+                WriteColor(),
+                WriteWeight(),
+                WriteProteins(),
+                WriteFats(),
+                WriteCarbohydrates(),
+                WriteCalories(),
+                WriteWater(),
+                WriteCa(),
+                WriteMg(),
+                WriteNa()
+                );
+        }
+        protected double WriteCa()
+        {
+            Console.WriteLine("Введите количество кальция (г): ");
+            return WriteDouble();
+        }
+        protected double WriteMg()
+        {
+            Console.WriteLine("Введите количество магния (г): ");
+            return WriteDouble();
+        }
+        protected double WriteNa()
+        {
+            Console.WriteLine("Введите количество натрия (г): ");
+            return WriteDouble();
+        }
+    }
+}
