ba9d67cfcac8ee84e04e1231ac12210bd55d6b3a
MANDOM <yourmarshall@yandex.by>
2020-12-06 14:54:39 +0300

    Реализован класс Carrot
    Класс был создан до этого, но из-за ошибки компиляции оставался пустым.

--- Chef/Chef/Vegetables/Roots/Carrot.cs
+++ Chef/Chef/Vegetables/Roots/Carrot.cs
@@ -4,7 +4,33 @@ using System.Text;
 
 namespace Chef.Vegetables.Roots
 {
-    class Carrot
+    class Carrot : Root
     {
+        double si;
+        public double Si { get; set; }
+        double mo;
+        public double Mo { get; set; }
+        double co;
+        public double Co { get; set; }
+        public Carrot(string sort, string color, double weight, double proteins, double fats, double carbohydrates, double calories, double water, double si, double mo, double co)
+            : base(sort, color, weight, proteins, fats, carbohydrates, calories, water)
+        {
+            this.si = si;
+            this.mo = mo;
+            this.co = co;
+        }
+        public override void GetInformation()
+        {
+            base.GetInformation();
+            Console.WriteLine("Минералы и витамины:\n" +
+                "Кремний - " + Si + " мг.\n" +
+                "Молибден - " + Mo + " мг.\n" +
+                "Оксид углерода - " + Co + " мг.");
+        }
+        public override void Handle()
+        {
+            base.Handle();
+            Console.WriteLine("Нарезали морковь...");
+        }
     }
 }
