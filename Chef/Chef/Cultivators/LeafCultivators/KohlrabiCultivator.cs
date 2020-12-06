03260b56374260c8b63bc05b59271d38b9a3b524
MANDOM <yourmarshall@yandex.by>
2020-12-06 21:24:32 +0300

    Создана фабрика листовых
    На основе абстрактного класса Cultivator был создан абстрактный класс LeafCultivator, отвечающий за создание объетов листов, и содержащий в себе метод CultivateLeaf, который обязаны переопределить фабрики конкретных объектов овощей, такие как: WhiteCabbageCultivator, BroccoliCultivator, KohlrabiCultivator.

---
+++ Chef/Chef/Cultivators/LeafCultivators/KohlrabiCultivator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chef.Cultivators.LeafCultivators
+{
+    class KohlrabiCultivator : LeafCultivator
+    {
+        public override Vegetables.Leafs.Leaf CultivateLeaf()
+        {
+            return new Vegetables.Leafs.Kohlrabi
+                (
+                WriteSort(),
+                WriteColor(),
+                WriteWeight(),
+                WriteProteins(),
+                WriteFats(),
+                WriteCarbohydrates(),
+                WriteCalories(),
+                WriteWater(),
+                WriteCa(),
+                WriteMg(),
+                WriteP()
+                );
+        }
+        protected double WriteCa()
+        {
+            Console.WriteLine("Введите количество кальция (г): ");
+            return WriteDouble();
+        }
+        protected double WriteMg()
+        {
+            Console.WriteLine("Введите количество магния (г): ");
+            return WriteDouble();
+        }
+        protected double WriteP()
+        {
+            Console.WriteLine("Введите количество фосфора (г): ");
+            return WriteDouble();
+        }
+    }
+}
