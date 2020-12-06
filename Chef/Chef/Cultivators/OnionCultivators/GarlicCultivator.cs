276b78da060fabe6ac9db5fd9676b1dea1ff0a71
MANDOM <yourmarshall@yandex.by>
2020-12-06 21:14:26 +0300

    Создана фабрика луковых
    На основе абстрактного класса Cultivator был создан абстрактный класс OnionCultivator, отвечающий за создание объетов луков, и содержащий в себе метод CultivateOnion, который обязаны переопределить фабрики конкретных объектов овощей, такие как: LeekOnionCultivator, NapiformOnionCultivator, GarlicCultivator.

---
+++ Chef/Chef/Cultivators/OnionCultivators/GarlicCultivator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chef.Cultivators.OnionCultivators
+{
+    class GarlicCultivator : OnionCultivator
+    {
+        public override Vegetables.Onions.Onion CultivateOnion()
+        {
+            return new Vegetables.Onions.Garlic
+                (
+                WriteSort(),
+                WriteColor(),
+                WriteWeight(),
+                WriteProteins(),
+                WriteFats(),
+                WriteCarbohydrates(),
+                WriteCalories(),
+                WriteWater(),
+                WriteCo(),
+                WriteCr(),
+                WriteMn()
+                );
+        }
+        protected double WriteCo()
+        {
+            Console.WriteLine("Введите количество оксида углерода (г): ");
+            return WriteDouble();
+        }
+        protected double WriteCr()
+        {
+            Console.WriteLine("Введите количество хрома (г): ");
+            return WriteDouble();
+        }
+        protected double WriteMn()
+        {
+            Console.WriteLine("Введите количество марганца (г): ");
+            return WriteDouble();
+        }
+    }
+}
