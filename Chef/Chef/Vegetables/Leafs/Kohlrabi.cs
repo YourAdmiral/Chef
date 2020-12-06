db4eafb2d6221333423ece52e275735e2c60f1f7
MANDOM <yourmarshall@yandex.by>
2020-12-06 15:11:40 +0300

    Реализован класс Kohlrabi
    Класс был создан до этого, но из-за ошибки компиляции оставался пустым.

--- Chef/Chef/Vegetables/Leafs/Kohlrabi.cs
+++ Chef/Chef/Vegetables/Leafs/Kohlrabi.cs
@@ -4,8 +4,33 @@ using System.Text;
 
 namespace Chef.Vegetables.Leafs
 {
-    class Kohlrabi
+    class Kohlrabi : Leaf
     {
-
+        double ca;
+        public double Ca { get; set; }
+        double mg;
+        public double Mg { get; set; }
+        double p;
+        public double P { get; set; }
+        public Kohlrabi(string sort, string color, double weight, double proteins, double fats, double carbohydrates, double calories, double water, double ca, double mg, double p)
+            : base(sort, color, weight, proteins, fats, carbohydrates, calories, water)
+        {
+            this.ca = ca;
+            this.mg = mg;
+            this.p = p;
+        }
+        public override void GetInformation()
+        {
+            base.GetInformation();
+            Console.WriteLine("Минералы и витамины:\n" +
+                "Кальций - " + Ca + " мг.\n" +
+                "Магний - " + Mg + " мг.\n" +
+                "Фосфор - " + P + " мг.");
+        }
+        public override void Handle()
+        {
+            base.Handle();
+            Console.WriteLine("Нашинковали кольраби...");
+        }
     }
 }
